adb0eb1941453b90cb8d17e4a7cdd1c1e94eec3e
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-11-15 12:23:11 -0300

    Cria interface Service

---
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/service/Service.java
@@ -0,0 +1,10 @@
+package br.edu.ifpb.pweb2.colegiplus.service;
+import java.util.List;
+public interface Service<T, ID> {
+    public List<T> findAll();
+
+    public T findById(ID id);
+
+    public T save(T entity);
+
+}
