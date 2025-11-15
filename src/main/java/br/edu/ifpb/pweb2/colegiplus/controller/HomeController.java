a43fc2cf7cd25f2b0fd0c9221b8f3e26e5d41181
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-11-15 12:25:19 -0300

    Cria HomeController

---
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.colegiplus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String showHomePage() {
+        return "index";
+    }
+
+}
